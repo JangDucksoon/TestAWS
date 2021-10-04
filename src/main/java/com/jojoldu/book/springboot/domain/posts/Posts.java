426f7f47ef29c552ca1f22ba02fada37b1f9553a
JangDucksoon <smaa030@naver.com>
2021-10-04 14:00:41 +0900

    머스테치 화면 목록조회 구현

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,15 +1,17 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
